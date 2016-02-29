bdf004ff3c598b0d19499629fec1c191b4002530
JF Beaulieu <JF Beaulieu>
2016-02-28 21:17:24 -0500

    removed folder

---
+++ client/app/app.constant.js
@@ -0,0 +1,9 @@
+(function(angular, undefined) {
+'use strict';
+
+angular.module('angularjsSeedOpenshiftApp.constants', [])
+
+.constant('appConfig', {userRoles:['guest','user','admin']})
+
+;
+})(angular);
